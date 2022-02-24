2cb518190f144ca5e2a7706b929019cf59336a1d
Eden <elsw@hotmail.co.uk>
2022-02-24 10:14:30 +0000

    create musicl player setp 2 complete

--- seed/player/music_library.py
+++ seed/player/music_library.py
@@ -1,2 +1,37 @@
+# from dbm.ndbm import library
+
+
 class MusicLibrary:
-    pass
+
+
+
+    def __init__(self):
+        self.song_library = []
+
+
+    def add(self, track):
+        self.track = track
+        self.song_library.append(self.track)
+
+    def remove(self, track):
+        self.track = track 
+        if self.track < len(self.song_library):
+            del self.song_library[self.track] 
+            return True
+        else:
+            return False
+            
+
+        
+
+    def all(self):
+        return self.song_library
+
+    # def remove(self, track):
+    #     self.track = track - 1
+    #     del self.song_library[self.track]       
+
+        
+
+
+# music_library = MusicLibrary()
--- seed/tests/test_music_library.py
+++ seed/tests/test_music_library.py
@@ -6,3 +6,39 @@ from player.music_library import MusicLibrary
 class TestMusicLibrary(unittest.TestCase):
     def test_constructs(self):
         MusicLibrary()
+
+    def test_addsongs(self):
+        music_library = MusicLibrary()
+        music_library.add("Rolling Blackouts by The Go! Team")
+        music_library.add("Oh Yeah by Locust")
+        music_library.add("Sleep on the Wing by Bibio")
+        self.assertEqual(music_library.song_library, ["Rolling Blackouts by The Go! Team", "Oh Yeah by Locust", "Sleep on the Wing by Bibio"])
+
+    
+
+
+    def test_remove_song(self):
+        music_library = MusicLibrary()
+        music_library.add("Rolling Blackouts by The Go! Team")
+        music_library.add("Oh Yeah by Locust")
+        music_library.add("Sleep on the Wing by Bibio")
+        music_library.remove(1)
+        self.assertEqual(music_library.song_library, ["Rolling Blackouts by The Go! Team", "Sleep on the Wing by Bibio"])
+    
+
+
+    def test_view_all_songs(self):
+        music_library = MusicLibrary()
+        music_library.add("Rolling Blackouts by The Go! Team")
+        music_library.add("Sleep on the Wing by Bibio")
+        self.assertEqual(music_library.all(), ["Rolling Blackouts by The Go! Team", "Sleep on the Wing by Bibio"])
+        
+
+
+    def test_returns_false_if_removing_song_not_in_list(self):
+        music_library = MusicLibrary()
+        self.assertFalse(music_library.remove(20))
+  
+
+
+
